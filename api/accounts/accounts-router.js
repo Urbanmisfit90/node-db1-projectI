139fd822d662286ed839cdc1aa45cf3bb8b6970c
Robert Dixon <robmsc24@gmail.com>
2024-03-30 02:18:19 -0400

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,45 +1,57 @@
 const router = require("express").Router();
+const md = require("./accounts-middleware");
 
 router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
   try {
-      res.json('get accounts')
+    res.json("get accounts");
   } catch (err) {
     next(err);
   }
 });
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", md.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json('get account by id')
+    res.json("get account by id");
   } catch (err) {
     next(err);
   }
 });
 
-router.post("/", (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json('post account')
-  } catch (err) {
-    next(err);
+router.post(
+  "/",
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("post account");
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.put("/:id", (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json('update account')
-  } catch (err) {
-    next(err);
+router.put(
+  "/:id",
+  md.checkAccountId,
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("update account");
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", md.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
   try {
-      res.json('delete account')
+    res.json("delete account");
   } catch (err) {
     next(err);
   }
