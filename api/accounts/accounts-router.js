6a44cb9887224d4f8216334ea076b8059710a313
Robert Dixon <robmsc24@gmail.com>
2024-04-01 14:43:47 -0400

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -38,9 +38,9 @@ router.put(
   "/:id",
   md.checkAccountId,
   md.checkAccountPayload,
-  md.checkAccountNameUnique,
-  (req, res, next) => {
-    // DO YOUR MAGIC
+  async (req, res, next) => {
+    const updated = await Account.updateById(req.params.id, req.body)
+    res.json(updated)
     try {
       res.json("update account");
     } catch (err) {
